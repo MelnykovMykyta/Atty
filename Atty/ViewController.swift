d11ff270cbdd08c1573aee8f508b891ea52e0773
MelnykovMykyta <melnikovnikita@ukr.net>
2023-10-20 13:35:14 +0300

    Initial Commit

---
+++ Atty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Atty
+//
+//  Created by Nikita Melnikov on 20.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
